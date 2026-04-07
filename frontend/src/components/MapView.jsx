38eae8b40e47ed6171881dc03275e39f7dd95bae
Pandu <karthiktamarapalli9@gmail.com>
2026-04-07 20:23:50 +0530

    Solution done till this end

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,9 +1,11 @@
-import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { useState } from "react";
 import "leaflet/dist/leaflet.css";
 
-function ClickHandler({ onClick }) {
+function ClickHandler({ onClick, setPosition }) {
   useMapEvents({
     click(e) {
+      setPosition(e.latlng);
       onClick(e.latlng);
     },
   });
@@ -11,16 +13,19 @@ function ClickHandler({ onClick }) {
 }
 
 export default function MapView({ onMapClick }) {
+  const [position, setPosition] = useState(null);
+
   return (
     <MapContainer
-      center={[20.5937, 78.9629]} // India center
+      center={[20.5937, 78.9629]}
       zoom={5}
       style={{ height: "100vh", width: "100%" }}
     >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <ClickHandler onClick={onMapClick} />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      <ClickHandler onClick={onMapClick} setPosition={setPosition} />
+
+      {position && <Marker position={position} />}
     </MapContainer>
   );
 }
--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -2,21 +2,37 @@ import { useState } from "react";
 import axios from "axios";
 import MapView from "../components/MapView";
 import InfoPanel from "../components/InfoPanel";
+import Loader from "../components/Loader";
 
 export default function MapPage() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
   const handleClick = async (latlng) => {
-    const res = await axios.get(
-      `http://localhost:8000/analysis?lat=${latlng.lat}&lon=${latlng.lng}`
-    );
-    setData(res.data);
-  };
+    setLoading(true);
+    setData(null);
 
+    try {
+      const res = await axios.get(
+        `${BASE_URL}/analysis?lat=${latlng.lat}&lon=${latlng.lng}`
+      );
+      console.log("API DATA:", res.data);
+
+      setData(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setLoading(false);
+  };
   return (
-    <div>
-      <MapView onMapClick={handleClick} />
-      {data && <InfoPanel data={data} />}
-    </div>
-  );
+  <div style={{ position: "relative" }}>
+    <MapView onMapClick={handleClick} />
+
+    {loading && <Loader />}
+    {data && <InfoPanel data={data} />}
+  </div>
+);
 }
