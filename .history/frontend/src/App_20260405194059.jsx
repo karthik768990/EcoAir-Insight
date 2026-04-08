9730fdaa8936ab4a5ca8972d3ece3b879ba77ebd
Pandu <karthiktamarapalli9@gmail.com>
2026-04-08 12:04:44 +0530

    SIngle page with the fade in animation done

--- .history/frontend/src/App_20260405194059.jsx
+++ .history/frontend/src/App_20260405194059.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 import MapPage from "./pages/MapPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MapPage />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/map" element={<MapPage />} />
       </Routes>
     </BrowserRouter>
   );
